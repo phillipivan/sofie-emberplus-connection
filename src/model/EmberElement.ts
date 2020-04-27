331f623eee4583c8b13f6b53f3dee1ade7c48638
Ola Christian Gundelsby <ola.christian.gundelsby@nrk.no>
2020-04-27 17:49:46 +0200

    feat: type predicate function for EmberElement interface

--- src/model/EmberElement.ts
+++ src/model/EmberElement.ts
@@ -1,4 +1,4 @@
-export { ElementType, EmberElement }
+export { ElementType, EmberElement, isEmberElement }
 
 enum ElementType {
 	Parameter = 'PARAMETER',
@@ -12,4 +12,22 @@ enum ElementType {
 interface EmberElement {
 	type: ElementType
 	number: number
+}
+
+function isEmberElement(obj: any): obj is EmberElement {
+	if (obj === undefined || obj === null) {
+		return false
+	}
+
+	const { type, number } = obj
+
+	if (!type || !Object.values(ElementType as any).includes(type)) {
+		return false
+	}
+
+	if (typeof number !== 'number') {
+		return false
+	}
+
+	return true
 }
