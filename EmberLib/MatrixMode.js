2107fe25d07cf4df5051c105102aef3f4d8f9f6a
Balte de Wit <balte.dewit@gmail.com>
2020-06-08 15:41:04 +0200

    chore: remove old project files

--- EmberLib/MatrixMode.js
+++
@@ -1,8 +0,0 @@
-const Enum = require('enum');
-
-const MatrixMode = new Enum({
-    linear: 0,
-    nonLinear: 1
-});
-
-module.exports = MatrixMode;
--- EmberServer/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    EmberServer: require("./EmberServer"),
-    ServerEvents: require("./ServerEvents")
-};
