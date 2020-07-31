babc51c7b9f328d36fc0e327dfde2a60255d1ee6
Balte de Wit <balte.dewit@gmail.com>
2020-07-31 14:24:46 +0200

    feat: emberplus provider

---
+++ src/Ember/Socket/S101Server.ts
@@ -0,0 +1,51 @@
+import { EventEmitter } from 'events'
+import { Socket, createServer, Server } from 'net'
+import S101Socket from './S101Socket'
+
+export class S101Server extends EventEmitter {
+	port: number
+	address: string | undefined
+	server: Server | null
+	status: string // TODO - enum
+
+	constructor(port: number, address?: string) {
+		super()
+		this.port = port
+		this.address = address
+		this.server = null
+		this.status = 'disconnected'
+	}
+
+	addClient(socket: Socket) {
+		// Wrap the tcp socket into an S101Socket.
+		const client = new S101Socket(socket)
+		this.emit('connection', client)
+	}
+
+	listen() {
+		return new Promise((resolve, reject) => {
+			if (this.status !== 'disconnected') {
+				return reject(new Error('Already listening'))
+			}
+			this.server = createServer((socket) => {
+				this.addClient(socket)
+			})
+				.on('error', (e) => {
+					this.emit('error', e)
+					if (this.status === 'disconnected') {
+						return reject(e)
+					}
+				})
+				.on('listening', () => {
+					this.emit('listening')
+					this.status = 'listening'
+					resolve()
+				})
+			this.server.listen(this.port, this.address)
+		})
+	}
+
+	discard() {
+		this.server?.close()
+	}
+}
