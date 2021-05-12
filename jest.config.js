a6d9aa5e58d86e58b3e835591708f1e129f6df30
Balte de Wit <contact@balte.nl>
2021-05-12 13:59:55 +0200

    chore: add project code standard preset

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 /*global module */
 module.exports = {
 	preset: 'ts-jest',
-	testEnvironment: 'node'
+	testEnvironment: 'node',
+	globals: {
+		'ts-jest': {
+			tsconfig: 'tsconfig.json',
+		},
+	},
 }
